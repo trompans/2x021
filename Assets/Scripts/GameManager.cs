d9edb3deda54838b1ad1c2689c93e14bbd2c6426
trompans <trompans@spam4.me>
2017-05-06 19:57:38 +0200

    Phase 1 Prueba de funcionalidad Unity - Github.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+    public float currentBalance;
+    public Text currentBalanceText;
+
+
+    // Use this for initialization
+    void Start () {
+        
+        currentBalanceText.text = currentBalance.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("es-es"));
+
+        //Distintas formas de dar formato a ToString... "https://msdn.microsoft.com/es-es/library/dwhawy9k(v=vs.110).aspx"
+        //currentBalanceText.text = currentBalance.ToString("C", System.Globalization.CultureInfo.CurrentCulture);
+        //Otra forma...
+        //currentBalanceText.text = currentBalance.ToString("##.## €");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    public void AddToBalance(float cantidad)
+    {
+        currentBalance += cantidad;
+        currentBalanceText.text = currentBalance.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("es-es"));
+
+    }
+
+    public bool CanBuy(float cantidadaGastar)
+    {
+        if (cantidadaGastar > currentBalance)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Store.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Store : MonoBehaviour {
+
+    // Public variables - Define Gameplay //
+    public float baseStoreCost;
+    public float baseStoreProfit;
+    public float storeTimer;
+    public int   storeCount;
+    public bool  managerUnlocked;
+    public float storeCostMultiplier;
+
+    public Text storeCountText;
+
+    public Slider progressSlider;
+
+    public Text buyButtonText;
+    public Button buyButton;
+
+    public Image fillColor;
+    private float nextStoreCost;
+
+    float currentTimer = 0;
+    bool startTimer;
+
+
+    // Use this for initialization
+    void Start () {
+
+        startTimer = false;
+        storeCountText.text = storeCount.ToString();
+        nextStoreCost = baseStoreCost;
+
+        fillColor.color = Color.clear;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (startTimer)
+        {
+            currentTimer += Time.deltaTime;
+            if (currentTimer >= storeTimer)
+            {
+                if (!managerUnlocked)
+                {
+                    startTimer = false;
+                    fillColor.color = Color.clear;
+                }
+                currentTimer = 0;
+                GameManager.instance.AddToBalance(baseStoreProfit * storeCount);
+            }
+        }
+        progressSlider.value = currentTimer/storeTimer;
+        CheckStoreBuy();
+		
+	}
+
+    // Own Methods...
+    public void CheckStoreBuy()
+    {
+        if (GameManager.instance.CanBuy(nextStoreCost))
+            buyButton.interactable = true;
+        else
+            buyButton.interactable = false;
+    }
+
+    public void BuyStoreOnClick()
+    {
+        if (!GameManager.instance.CanBuy(nextStoreCost))
+        {
+            return;
+        }
+        storeCount++;
+        storeCountText.text = storeCount.ToString();
+        GameManager.instance.AddToBalance(-nextStoreCost);
+        nextStoreCost = (baseStoreCost * Mathf.Pow(storeCostMultiplier, storeCount));
+        buyButtonText.text = "¡Compra! " + nextStoreCost.ToString("##.## €");
+        Debug.Log(nextStoreCost);
+    }
+
+    public void StoreOnClick()
+    {
+        if (!startTimer && storeCount > 0)
+        {
+            startTimer = true;
+            fillColor.color = Color.green;
+        }
+    }
+}
