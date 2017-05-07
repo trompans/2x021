51caff6f73a284928c89b3f44552ae4b5b8c974f
trompans <trompans@users.noreply.github.com>
2017-05-07 03:13:22 +0200

    pequeños cambios

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public static GameManager instance;
     public float currentBalance;
-    public Text currentBalanceText;
+ 
 
 
     // Use this for initialization
     void Start () {
         
-        currentBalanceText.text = currentBalance.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("es-es"));
-
-        //Distintas formas de dar formato a ToString... "https://msdn.microsoft.com/es-es/library/dwhawy9k(v=vs.110).aspx"
-        //currentBalanceText.text = currentBalance.ToString("C", System.Globalization.CultureInfo.CurrentCulture);
-        //Otra forma...
-        //currentBalanceText.text = currentBalance.ToString("##.## €");
-
     }
 
     // Update is called once per frame
@@ -36,8 +28,7 @@ public class GameManager : MonoBehaviour {
     public void AddToBalance(float cantidad)
     {
         currentBalance += cantidad;
-        currentBalanceText.text = currentBalance.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("es-es"));
-
+        UIManager.instance.UpdateUI();
     }
 
     public bool CanBuy(float cantidadaGastar)
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
     public static UIManager instance;
+    public Text currentBalanceText;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
@@ -20,5 +22,16 @@ public class UIManager : MonoBehaviour {
     {
         if (instance == null)
             instance = this;
+    }
+
+    public void UpdateUI()
+    {
+        currentBalanceText.text = GameManager.instance.currentBalance.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("es-es"));
+
+        //Distintas formas de dar formato a ToString... "https://msdn.microsoft.com/es-es/library/dwhawy9k(v=vs.110).aspx"
+        //currentBalanceText.text = currentBalance.ToString("C", System.Globalization.CultureInfo.CurrentCulture);
+        //Otra forma...
+        //currentBalanceText.text = currentBalance.ToString("##.## €");
+
     }
 }
