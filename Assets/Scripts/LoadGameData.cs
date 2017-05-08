409a48d93fe9c7c454209613b8183ea224ab3611
trompans <trompans@users.noreply.github.com>
2017-05-08 20:33:27 +0200

    Con errores

---
+++ Assets/Scripts/LoadGameData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class LoadGameData : MonoBehaviour {
+
+    public TextAsset gameData;
+    public void Start()
+    {
+        Invoke("LoadData", 0.5f);
+    }
+
+    public void LoadData()
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(gameData.text);
+        XmlNodeList storeList = 
+    }
+
+}
